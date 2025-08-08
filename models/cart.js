235f27cbbde7adc3211b50298cbdb8ce35d1b18e
Sri Vishnu <srivishp@gmail.com>
2025-08-08 18:24:56 +0530

    Update code

--- models/cart.js
+++ models/cart.js
@@ -9,7 +9,7 @@ const p = path.join(
 
 module.exports = class Cart {
   static addProduct(id, productPrice) {
-    // Fetch previous cart
+    // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
@@ -36,6 +36,37 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(cart), (err) => {
         console.log(err);
       });
+    });
+  }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find((prod) => prod.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        (prod) => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 };
--- routes/shop.js
+++ routes/shop.js
@@ -8,21 +8,22 @@ const router = express.Router();
 
 //# Specific routes take precedence over dynamic routes
 //? Eg: Deleting a product is parsed first/takes priority over looking up a product based on a dynamic ID
-
 router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
 // * Getting the ID using :
 // -> It indicates not to search for a route but search for a param
-router.get("/products/:productId", shopController.getProductById);
+router.get("/products/:productId", shopController.getProduct);
 
 router.get("/cart", shopController.getCart);
+
+router.post("/cart", shopController.postCart);
+
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
 router.get("/orders", shopController.getOrders);
 
 router.get("/checkout", shopController.getCheckout);
-
-router.post("/cart", shopController.postCart);
 
 module.exports = router;
